46f9778b27e80dbda4dcd0e80bd08f44afb1530e
bookian <bookian@gmail.com>
2017-03-11 21:01:13 +0800

    added /todos and /todos/:id

--- server.js
+++ server.js
@@ -2,9 +2,40 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id: 1,
+    description: 'Learn nodejs',
+    completed: false
+}, {
+    id: 2,
+    description: 'Learn Go!',
+    completed: false
+},{
+    id: 3,
+    description: 'Buy eggs',
+    completed: true
+}];
+
 app.get('/', function (req, res) {
     res.send('TODO API root');
 });
+
+// GET /todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == todoId) {
+            res.json(todos[i]);
+            return;
+        }
+    }
+    res.status(404).send();
+})
 
 app.listen(PORT, function () {
     console.log('Express listening on port: ' + PORT + '...');
