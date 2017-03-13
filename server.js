0caca7912f0a2c4bcbbe54cd861f68922bd90b84
bookian <bookian@gmail.com>
2017-03-13 11:53:23 +0800

    added validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require("underscore");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -21,20 +22,25 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    for (var i = 0; i < todos.length; i++) {
-        if (todos[i].id == todoId) {
-            res.json(todos[i]);
-            return;
-        }
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    if (matchedTodo) {
+        return res.json(matchedTodo);
+    } else {
+        res.status(404).send();
     }
-    res.status(404).send();
 });
 
 // POST /todos
 app.post('/todos', function (req, res) {
-    var body = req.body;
-    body.id = todoNextId++;
+    var body = _.pick(req.body, 'description', 'completed');
 
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.id = todoNextId++;
+    body.description = body.description.trim();
+    
     todos.push(body);
     res.json(body);
 })
