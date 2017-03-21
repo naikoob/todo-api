962bae90ea7cc90e218a89ea80acc4957b319d33
Boo Kian <bookian@bigmac.local>
2017-03-22 00:13:37 +0800

    hash and salt password

--- server.js
+++ server.js
@@ -117,14 +117,14 @@ app.post('/users', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
     db.user.create(body).then(function (user) {
-        res.json(user.toJSON());
+        res.json(user.toPublicJSON());
     }, function (e) {
         res.status(400).json(e.errors);
     });
 });
 
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({ force: false }).then(function () {
     app.listen(PORT, function () {
         console.log('Express listening on port: ' + PORT + '...');
     });
