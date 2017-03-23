3050233d8d9e17dc6952f8500f62af48bedfe939
Boo Kian <bookian@gmail.com>
2017-03-23 13:43:22 +0800

    added logout and token model

--- middleware.js
+++ middleware.js
@@ -1,14 +1,26 @@
+var cryptojs = require('crypto-js');
+
 module.exports = function (db) {
     return {
         requireAuthentication: function (req, res, next) {
-            var token = req.get('Auth');
+            var token = req.get('Auth') || '';
 
-            db.user.findByToken(token).then(function (user) {
+            db.token.findOne({
+                where: {
+                    token_hash: cryptojs.MD5(token).toString()
+                }
+            }).then(function (tokenInstance) {
+                if (!tokenInstance) {
+                    throw new Error();
+                }
+                req.token = tokenInstance;
+                return db.user.findByToken(token);
+            }).then(function (user) {
                 req.user = user;
                 next();
-            }, function() {
+            }).catch(function () {
                 res.status(401).send();
-            });
+            })
         }
     };
 };
