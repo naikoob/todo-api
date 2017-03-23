83095bb9b2d47dab07a9eacb4465a8f1488975ee
Boo Kian <bookian@gmail.com>
2017-03-23 11:20:46 +0800

    add token generation to POST /users/login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function (sequelize, DataTypes) {
     var user = sequelize.define('user', {
@@ -33,42 +35,62 @@ module.exports = function (sequelize, DataTypes) {
             }
         }
     }, {
-        hooks: {
-            beforeValidate: function (user, options) {
-                if (typeof user.email === 'string') {
-                    user.email = user.email.toLowerCase();
-                }
-            }
-        },
-        classMethods: {
-            authenticate: function (body) {
-                return new Promise(function (resolve, reject) {
-                    if (typeof body.email !== 'string' || typeof body.password !== 'string') {
-                        return reject();
+            hooks: {
+                beforeValidate: function (user, options) {
+                    if (typeof user.email === 'string') {
+                        user.email = user.email.toLowerCase();
                     }
-
-                    user.findOne({
-                        where: {
-                            email: body.email
-                        }
-                    }).then(function (user) {
-                        if (!user || !bcrypt.compareSync(body.password, user.password_hash)) {
+                }
+            },
+            classMethods: {
+                authenticate: function (body) {
+                    return new Promise(function (resolve, reject) {
+                        if (typeof body.email !== 'string' || typeof body.password !== 'string') {
                             return reject();
                         }
-                        resolve(user);
-                    }, function (e) {
-                        reject();
+
+                        user.findOne({
+                            where: {
+                                email: body.email
+                            }
+                        }).then(function (user) {
+                            if (!user || !bcrypt.compareSync(body.password, user.password_hash)) {
+                                return reject();
+                            }
+                            resolve(user);
+                        }, function (e) {
+                            reject();
+                        });
                     });
-                });
+                }
+            },
+            instanceMethods: {
+                toPublicJSON: function () {
+                    var json = this.toJSON();
+                    return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+                },
+                generateToken: function (type) {
+                    if (!_.isString(type)) {
+                        return undefined;
+                    }
+
+                    try {
+                        var stringData = JSON.stringify({
+                            id: this.get('id'),
+                            type: type
+                        });
+                        var encrypteData = cryptojs.AES.encrypt(stringData, 'abc123!@#').toString();
+                        var token = jwt.sign({
+                            token: encrypteData
+                        }, 'qwerty1234%');
+                        return token;
+                    } catch (e) {
+                        console.error(e);
+                        return undefined;
+                    }
+                }
             }
-        },
-        instanceMethods: {
-            toPublicJSON: function () {
-                var json = this.toJSON();
-                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
-            }
-        }
-    });
+        });
 
     return user;
 }
